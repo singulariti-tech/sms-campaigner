056dac9b3402cfc944e831f1d7714d370e5aadaa
John Emmanuel <johnemmanueljd@gmail.com>
2009-12-05 11:12:16 +0530

    MODIFIED: Rewrote DI based on XML instead of annotations

--- src/com/alteregos/sms/campaigner/services/IncomingCallService.java
+++ src/com/alteregos/sms/campaigner/services/IncomingCallService.java
@@ -4,20 +4,18 @@ import com.alteregos.sms.campaigner.data.dao.IncomingCallDao;
 import com.alteregos.sms.campaigner.data.dto.IncomingCall;
 import com.alteregos.sms.campaigner.data.dto.IncomingCallPk;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Service;
 
 /**
  *
  * @author John
  */
-@Service("incomingCallService")
 public class IncomingCallService {
 
-    @Autowired(required = true)
-    @Qualifier("sqliteIncomingCallDao")
     private IncomingCallDao incomingCallDao;
+
+    public void setIncomingCallDao(IncomingCallDao incomingCallDao) {
+        this.incomingCallDao = incomingCallDao;
+    }
 
     public List<IncomingCall> findAll() {
         return incomingCallDao.findAll();
