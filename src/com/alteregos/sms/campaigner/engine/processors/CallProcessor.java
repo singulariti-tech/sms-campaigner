7355fe25b27eabda6fbddc73a5360c83e8a39b28
John Emmanuel <johnemmanueljd@gmail.com>
2009-12-05 12:04:40 +0530

    MODIFIED: Rewrote code using spring beans instead of JPA

--- src/com/alteregos/sms/campaigner/engine/processors/CallProcessor.java
+++ src/com/alteregos/sms/campaigner/engine/processors/CallProcessor.java
@@ -1,17 +1,13 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.alteregos.sms.campaigner.engine.processors;
 
-import com.alteregos.sms.campaigner.data.beans.Calls;
-import com.alteregos.sms.campaigner.data.beans.Outbox;
+import com.alteregos.sms.campaigner.Main;
+import com.alteregos.sms.campaigner.data.dto.IncomingCall;
+import com.alteregos.sms.campaigner.data.dto.OutgoingMessage;
 import com.alteregos.sms.campaigner.engine.IProcessor;
+import com.alteregos.sms.campaigner.services.IncomingCallService;
 import com.alteregos.sms.campaigner.util.LoggerHelper;
 import java.util.ArrayList;
 import java.util.List;
-import javax.persistence.EntityManager;
-import javax.persistence.Query;
 import javax.persistence.RollbackException;
 import org.apache.log4j.Logger;
 
@@ -22,34 +18,28 @@ import org.apache.log4j.Logger;
 public class CallProcessor implements IProcessor {
 
     private static Logger log = LoggerHelper.getLogger();
-    private EntityManager entityManager;
-    private Query callsQuery;
     private String notificationMessage;
+    private IncomingCallService callService;
 
     public CallProcessor() {
-        entityManager = javax.persistence.Persistence.createEntityManagerFactory("absolute-smsPU").createEntityManager();
-        callsQuery = entityManager.createQuery("SELECT c FROM Calls c");
+        callService = Main.getApplication().getBean("incomingCallService");
     }
 
     @Override
     public void process() {
         log.debug("Processing calls...");
-        List<Calls> callsList = filter(callsQuery.getResultList());
-        List<Outbox> bulkSms = new ArrayList<Outbox>();
-        for (Calls call : callsList) {
-            Outbox outbox = new Outbox();
+        List<IncomingCall> callsList = filter(callService.findAll());
+        List<OutgoingMessage> bulkSms = new ArrayList<OutgoingMessage>();
+        for (IncomingCall call : callsList) {
+            OutgoingMessage outbox = new OutgoingMessage();
             outbox.setContent(notificationMessage);
-            outbox.setRecepient(call.getCaller());
+            outbox.setRecepientNo(call.getCallerNo());
             bulkSms.add(outbox);
             call.setProcess(true);
         }
 
         try {
-            entityManager.getTransaction().begin();
-            for (Outbox sms : bulkSms) {
-                entityManager.persist(sms);
-            }
-            entityManager.getTransaction().commit();
+            //TODO Insert or update calls appropriately
         } catch (RollbackException rollbackException) {
             log.error("Exception when processing Calls");
             log.error(rollbackException);
@@ -64,10 +54,10 @@ public class CallProcessor implements IProcessor {
         this.notificationMessage = notificationMessage;
     }
 
-    private List<Calls> filter(List<Calls> inputCalls) {
-        List<Calls> outputCalls = new ArrayList<Calls>();
-        for (Calls call : inputCalls) {
-            if (!call.getProcess()) {
+    private List<IncomingCall> filter(List<IncomingCall> inputCalls) {
+        List<IncomingCall> outputCalls = new ArrayList<IncomingCall>();
+        for (IncomingCall call : inputCalls) {
+            if (!call.isProcess()) {
                 outputCalls.add(call);
             }
         }
