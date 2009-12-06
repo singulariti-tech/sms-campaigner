68a488c8351c5fc38e3eb052bfed3880ff674ea1
johnemmanuel <johnemmanuel@localhost>
2009-12-06 13:02:09 +0530

    ADDED: Spring support

--- src/com/alteregos/sms/campaigner/engine/senders/SmsSender.java
+++ src/com/alteregos/sms/campaigner/engine/senders/SmsSender.java
@@ -1,19 +1,15 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.alteregos.sms.campaigner.engine.senders;
 
-import com.alteregos.sms.campaigner.data.beans.Outbox;
+import com.alteregos.sms.campaigner.Main;
 import com.alteregos.sms.campaigner.engine.ISender;
 import com.alteregos.sms.campaigner.util.LoggerHelper;
 import com.alteregos.sms.campaigner.business.MessagePriority;
 import com.alteregos.sms.campaigner.business.MessageStatus;
+import com.alteregos.sms.campaigner.data.dto.OutgoingMessage;
+import com.alteregos.sms.campaigner.services.ContactService;
+import com.alteregos.sms.campaigner.services.MessageService;
 import java.util.ArrayList;
 import java.util.List;
-import javax.persistence.EntityManager;
-import javax.persistence.Query;
-import javax.persistence.RollbackException;
 import org.apache.log4j.Logger;
 import org.smslib.MessagePriorities;
 import org.smslib.OutboundMessage;
@@ -27,13 +23,12 @@ public class SmsSender implements ISender {
 
     private static Logger log = LoggerHelper.getLogger();
     private Service service;
-    private EntityManager entityManager;
-    private Query outboxQuery;
+    private MessageService messageService;
 
     public SmsSender() {
-        entityManager = javax.persistence.Persistence.createEntityManagerFactory("absolute-smsPU").createEntityManager();
-        outboxQuery = entityManager.createQuery("SELECT o FROM Outbox o WHERE o.status =  ?1");
-        outboxQuery.setParameter(1, MessageStatus.UNSENT.toString());
+        messageService = Main.getApplication().getBean("messageService");
+        //outboxQuery = entityManager.createQuery("SELECT o FROM OutgoingMessage o WHERE o.status =  ?1");
+        //outboxQuery.setParameter(1, MessageStatus.UNSENT.toString());
     }
 
     public SmsSender(Service service) {
@@ -41,41 +36,42 @@ public class SmsSender implements ISender {
         this.service = service;
     }
 
+    @Override
     public void send() {
         log.debug("Starting to queue messages");
-        List<Outbox> messagesToBeSent = getMessagesToBeSent();
-        List<Outbox> highPriorityList = getMessagesOfPriority(messagesToBeSent, MessagePriority.HIGH);
+        List<OutgoingMessage> messagesToBeSent = getMessagesToBeSent();
+        List<OutgoingMessage> highPriorityList = getMessagesOfPriority(messagesToBeSent, MessagePriority.HIGH);
         messagesToBeSent.removeAll(highPriorityList);
-        List<Outbox> normalPriorityList = getMessagesOfPriority(messagesToBeSent, MessagePriority.NORMAL);
+        List<OutgoingMessage> normalPriorityList = getMessagesOfPriority(messagesToBeSent, MessagePriority.NORMAL);
         messagesToBeSent.removeAll(normalPriorityList);
-        List<Outbox> lowPriorityList = getMessagesOfPriority(messagesToBeSent, MessagePriority.LOW);
+        List<OutgoingMessage> lowPriorityList = getMessagesOfPriority(messagesToBeSent, MessagePriority.LOW);
         messagesToBeSent.removeAll(lowPriorityList);
         queuePriorityList(highPriorityList, MessagePriorities.HIGH);
         queuePriorityList(normalPriorityList, MessagePriorities.NORMAL);
         queuePriorityList(lowPriorityList, MessagePriorities.LOW);
     }
 
+    @Override
     public void setService(Service service) {
         this.service = service;
     }
 
-    private List<Outbox> getMessagesOfPriority(List<Outbox> inputList, MessagePriority priority) {
-        List<Outbox> outputList = new ArrayList<Outbox>();
-        String priorityString = priority.toString();
-        for (Outbox message : inputList) {
-            if (message.getPriority().equals(priorityString)) {
+    private List<OutgoingMessage> getMessagesOfPriority(List<OutgoingMessage> inputList, MessagePriority priority) {
+        List<OutgoingMessage> outputList = new ArrayList<OutgoingMessage>();
+        for (OutgoingMessage message : inputList) {
+            if (message.getPriority().equals(priority)) {
                 outputList.add(message);
             }
         }
         return outputList;
     }
 
-    private void queuePriorityList(List<Outbox> outboxList, MessagePriorities priority) {
+    private void queuePriorityList(List<OutgoingMessage> outboxList, MessagePriorities priority) {
         for (int i = 0; i < outboxList.size(); i++) {
-            Outbox message = outboxList.get(i);
-            message.setStatus(MessageStatus.QUEUED.toString());
-            OutboundMessage outboundMessage = new OutboundMessage(message.getRecepient(), message.getContent());
-            outboundMessage.setId(String.valueOf(message.getOutboxId()));
+            OutgoingMessage message = outboxList.get(i);
+            message.setStatus(MessageStatus.QUEUED);
+            OutboundMessage outboundMessage = new OutboundMessage(message.getRecepientNo(), message.getContent());
+            outboundMessage.setId(String.valueOf(message.getOutgoingMessageId()));
             outboundMessage.setPriority(priority);
 
             //UNCOMMENT THE FOLLOWING FOR MORE FEATURES
@@ -92,30 +88,15 @@ public class SmsSender implements ISender {
             this.service.queueMessage(outboundMessage);
 
             try {
-                entityManager.getTransaction().begin();
-                entityManager.getTransaction().commit();
-            } catch (RollbackException rollbackException) {
+                messageService.updateOutgoingMessage(message);
+            } catch (Exception rollbackException) {
                 log.debug("Error when queueing messages");
                 log.debug(rollbackException);
             }
         }
     }
 
-    private List<Outbox> getMessagesToBeSent() {
-        List<Outbox> input = outboxQuery.getResultList();
-        for (Outbox sms : input) {
-            entityManager.refresh(sms);
-        }
-        return filter(input);
-    }
-
-    private List<Outbox> filter(List<Outbox> inputList) {
-        List<Outbox> outputList = new ArrayList<Outbox>();
-        for (Outbox sms : inputList) {
-            if (sms.getStatus().equals(MessageStatus.UNSENT.toString())) {
-                outputList.add(sms);
-            }
-        }
-        return outputList;
+    private List<OutgoingMessage> getMessagesToBeSent() {
+        return messageService.getOutgoingMessages(MessageStatus.UNSENT);
     }
 }
