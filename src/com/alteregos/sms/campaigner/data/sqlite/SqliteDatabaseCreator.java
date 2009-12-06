e21d8b3ad46f636847501e0670d41a500309e947
johnemmanuel <johnemmanuel@localhost>
2009-12-06 22:34:52 +0530

    MODIFIED: Formatted code

--- src/com/alteregos/sms/campaigner/data/sqlite/SqliteDatabaseCreator.java
+++ src/com/alteregos/sms/campaigner/data/sqlite/SqliteDatabaseCreator.java
@@ -105,49 +105,111 @@ public class SqliteDatabaseCreator extends BaseSqliteDao implements DatabaseCrea
 
     public void createIncomingCallTable() {
         System.out.println("Creating incoming call table");
-        String sql = "CREATE TABLE IF NOT EXISTS [incoming_call] (" + "	[call_id] INTEGER PRIMARY KEY ASC ON CONFLICT FAIL AUTOINCREMENT UNIQUE ON CONFLICT FAIL, " + "	[receipt_date] DATETIME NOT NULL ON CONFLICT FAIL, " + "	[gateway_id] VARCHAR(64) NOT NULL ON CONFLICT FAIL, " + "	[caller_no] VARCHAR(16) NOT NULL ON CONFLICT FAIL, " + "	[process] BOOLEAN NOT NULL DEFAULT 'false'" + ")";
+        String sql = "CREATE TABLE IF NOT EXISTS [incoming_call] (" +
+                "	[call_id] INTEGER PRIMARY KEY ASC ON CONFLICT FAIL AUTOINCREMENT UNIQUE ON CONFLICT FAIL, " +
+                "	[receipt_date] DATETIME NOT NULL ON CONFLICT FAIL, " +
+                "	[gateway_id] VARCHAR(64) NOT NULL ON CONFLICT FAIL, " +
+                "	[caller_no] VARCHAR(16) NOT NULL ON CONFLICT FAIL, " +
+                "	[process] BOOLEAN NOT NULL DEFAULT 'false'" +
+                ")";
         jdbcTemplate.update(sql);
     }
 
     public void createIncomingMessageTable() {
         System.out.println("Creating incoming message table");
-        String sql = "CREATE TABLE IF NOT EXISTS [incoming_message] (" + "	[incoming_message_id] INTEGER PRIMARY KEY ASC ON CONFLICT FAIL AUTOINCREMENT UNIQUE ON CONFLICT FAIL, " + "	[process] BOOLEAN NOT NULL ON CONFLICT FAIL DEFAULT 'false', " + "	[sender_no] VARCHAR(16) NOT NULL ON CONFLICT FAIL, " + "	[encoding] CHAR(1) NOT NULL DEFAULT 7, " + "	[message_date] DATETIME NOT NULL ON CONFLICT FAIL, " + "	[receipt_date] DATETIME NOT NULL ON CONFLICT FAIL, " + "	[content] VARCHAR(160) NOT NULL ON CONFLICT FAIL, " + "	[message_type] CHAR(1) NOT NULL ON CONFLICT FAIL" + ")";
+        String sql = "CREATE TABLE IF NOT EXISTS [incoming_message] (" +
+                "	[incoming_message_id] INTEGER PRIMARY KEY ASC ON CONFLICT FAIL AUTOINCREMENT UNIQUE ON CONFLICT FAIL, " +
+                "	[process] BOOLEAN NOT NULL ON CONFLICT FAIL DEFAULT 'false', " +
+                "	[sender_no] VARCHAR(16) NOT NULL ON CONFLICT FAIL, " +
+                "	[encoding] CHAR(1) NOT NULL DEFAULT 7, " +
+                "	[message_date] DATETIME NOT NULL ON CONFLICT FAIL, " +
+                "	[receipt_date] DATETIME NOT NULL ON CONFLICT FAIL, " +
+                "	[content] VARCHAR(160) NOT NULL ON CONFLICT FAIL, " +
+                "	[message_type] CHAR(1) NOT NULL ON CONFLICT FAIL, " +
+                "       [gateway_id] VARCHAR(64)" +
+                ")";
         jdbcTemplate.update(sql);
     }
 
     public void createOutgoingMessageTable() {
         System.out.println("Creating outgoing message table");
-        String sql = "CREATE TABLE IF NOT EXISTS [outgoing_message] (" + "	[outgoing_message_id] INTEGER PRIMARY KEY ASC ON CONFLICT FAIL AUTOINCREMENT UNIQUE ON CONFLICT FAIL, " + "	[recepient_no] VARCHAR(16) NOT NULL ON CONFLICT FAIL, " + "	[sender_no] INTEGER(16) NOT NULL ON CONFLICT FAIL, " + "	[content] VARCHAR(640) NOT NULL ON CONFLICT FAIL, " + "	[encoding] CHAR(1) NOT NULL ON CONFLICT FAIL DEFAULT 7, " + "	[status_report] BOOLEAN NOT NULL ON CONFLICT FAIL DEFAULT 'false', " + "	[flash_message] BOOLEAN NOT NULL ON CONFLICT FAIL DEFAULT 'false', " + "	[src_port] INT(3) NOT NULL ON CONFLICT FAIL DEFAULT '-1', " + "	[dst_port] INT(3) NOT NULL ON CONFLICT FAIL DEFAULT '-1', " + "	[created_date] DATETIME NOT NULL ON CONFLICT FAIL, " + "	[sent_date] DATETIME, [ref_no] VARCHAR(64), " + "	[priority] CHAR(1) NOT NULL ON CONFLICT FAIL DEFAULT 'N', " + "	[status] CHAR(1) NOT NULL ON CONFLICT FAIL DEFAULT 'U', " + "	[errors] INT(3) NOT NULL ON CONFLICT FAIL DEFAULT 0, " + "	[gateway_id] VARCHAR(64), " + "	[message_type] CHAR(1) NOT NULL ON CONFLICT FAIL DEFAULT 'B'" + ")";
+        String sql = "CREATE TABLE IF NOT EXISTS [outgoing_message] (" +
+                "	[outgoing_message_id] INTEGER PRIMARY KEY ASC ON CONFLICT FAIL AUTOINCREMENT UNIQUE ON CONFLICT FAIL, " +
+                "	[recepient_no] VARCHAR(16) NOT NULL ON CONFLICT FAIL, " +
+                "	[sender_no] INTEGER(16), " +
+                "	[content] VARCHAR(640) NOT NULL ON CONFLICT FAIL, " +
+                "	[encoding] CHAR(1) NOT NULL ON CONFLICT FAIL DEFAULT 7, " +
+                "	[status_report] BOOLEAN NOT NULL ON CONFLICT FAIL DEFAULT 'false', " +
+                "	[flash_message] BOOLEAN NOT NULL ON CONFLICT FAIL DEFAULT 'false', " +
+                "	[src_port] INT(3) NOT NULL ON CONFLICT FAIL DEFAULT '-1', " +
+                "	[dst_port] INT(3) NOT NULL ON CONFLICT FAIL DEFAULT '-1', " +
+                "	[created_date] DATETIME NOT NULL ON CONFLICT FAIL, " +
+                "	[sent_date] DATETIME, [ref_no] VARCHAR(64), " +
+                "	[priority] CHAR(1) NOT NULL ON CONFLICT FAIL DEFAULT 'N', " +
+                "	[status] CHAR(1) NOT NULL ON CONFLICT FAIL DEFAULT 'U', " +
+                "	[errors] INT(3) NOT NULL ON CONFLICT FAIL DEFAULT 0, " +
+                "	[gateway_id] VARCHAR(64), " +
+                "	[message_type] CHAR(1) NOT NULL ON CONFLICT FAIL DEFAULT 'B'" +
+                ")";
         jdbcTemplate.update(sql);
     }
 
     public void createAutoReplyRuleTable() {
         System.out.println("Creating auto reply rule table");
-        String sql = "CREATE TABLE IF NOT EXISTS [auto_reply_rule] (" + "	[auto_reply_rule_id] INTEGER PRIMARY KEY ASC ON CONFLICT FAIL AUTOINCREMENT UNIQUE ON CONFLICT FAIL, " + "	[primary_keyword] VARCHAR(45) NOT NULL ON CONFLICT FAIL, " + "	[secondary_keyword] VARCHAR(45) NOT NULL ON CONFLICT FAIL, " + "	[enabled] BOOLEAN NOT NULL ON CONFLICT FAIL DEFAULT 'true', " + "	[content] VARCHAR(160) NOT NULL ON CONFLICT FAIL, " + "	[created_date] DATETIME NOT NULL ON CONFLICT FAIL, " + "	[modified_date] DATETIME NOT NULL ON CONFLICT FAIL, " + "	CONSTRAINT \"UNIQUE_PRIMARY_SECONDARY\" UNIQUE ([secondary_keyword], [primary_keyword]) ON CONFLICT FAIL" + ")";
+        String sql = "CREATE TABLE IF NOT EXISTS [auto_reply_rule] (" +
+                "	[auto_reply_rule_id] INTEGER PRIMARY KEY ASC ON CONFLICT FAIL AUTOINCREMENT UNIQUE ON CONFLICT FAIL, " +
+                "	[primary_keyword] VARCHAR(45) NOT NULL ON CONFLICT FAIL, " +
+                "	[secondary_keyword] VARCHAR(45) NOT NULL ON CONFLICT FAIL, " +
+                "	[enabled] BOOLEAN NOT NULL ON CONFLICT FAIL DEFAULT 'true', " +
+                "	[content] VARCHAR(160) NOT NULL ON CONFLICT FAIL, " +
+                "	[created_date] DATETIME NOT NULL ON CONFLICT FAIL, " +
+                "	[modified_date] DATETIME NOT NULL ON CONFLICT FAIL, " +
+                "	CONSTRAINT \"UNIQUE_PRIMARY_SECONDARY\" UNIQUE ([secondary_keyword], [primary_keyword]) ON CONFLICT FAIL" +
+                ")";
         jdbcTemplate.update(sql);
     }
 
     public void createContactTable() {
         System.out.println("Creating contact table");
-        String sql = "CREATE TABLE IF NOT EXISTS [contact] (" + "	[contact_id] INTEGER PRIMARY KEY ASC ON CONFLICT FAIL AUTOINCREMENT UNIQUE ON CONFLICT FAIL, " + "	[name] VARCHAR(64) NOT NULL ON CONFLICT FAIL, " + "	[mobile_no] VARCHAR(16) NOT NULL ON CONFLICT FAIL, " + "	[email] VARCHAR(64) NOT NULL ON CONFLICT FAIL, " + "	[address] VARCHAR(512) NOT NULL ON CONFLICT FAIL, " + "	CONSTRAINT \"UNIQUE_MOBILE_NO\" UNIQUE ([mobile_no]) ON CONFLICT FAIL" + ")";
+        String sql = "CREATE TABLE IF NOT EXISTS [contact] (" +
+                "	[contact_id] INTEGER PRIMARY KEY ASC ON CONFLICT FAIL AUTOINCREMENT UNIQUE ON CONFLICT FAIL, " +
+                "	[name] VARCHAR(64) NOT NULL ON CONFLICT FAIL, " +
+                "	[mobile_no] VARCHAR(16) NOT NULL ON CONFLICT FAIL, " +
+                "	[email] VARCHAR(64) NOT NULL ON CONFLICT FAIL, " +
+                "	[address] VARCHAR(512) NOT NULL ON CONFLICT FAIL, " +
+                "	CONSTRAINT \"UNIQUE_MOBILE_NO\" UNIQUE ([mobile_no]) ON CONFLICT FAIL" +
+                ")";
         jdbcTemplate.update(sql);
     }
 
     public void createDndTable() {
         System.out.println("Creating dnd table");
-        String sql = "CREATE TABLE IF NOT EXISTS [dnd] (" + "	[dnd_id] INTEGER PRIMARY KEY ASC ON CONFLICT FAIL AUTOINCREMENT UNIQUE ON CONFLICT FAIL, " + "	[registered_date] DATETIME NOT NULL ON CONFLICT FAIL, " + "	[mobile_no] VARCHAR(16) NOT NULL ON CONFLICT FAIL, " + "	CONSTRAINT \"UNIQUE_MOBILE_NO\" UNIQUE ([mobile_no]) ON CONFLICT FAIL" + ")";
+        String sql = "CREATE TABLE IF NOT EXISTS [dnd] (" +
+                "	[dnd_id] INTEGER PRIMARY KEY ASC ON CONFLICT FAIL AUTOINCREMENT UNIQUE ON CONFLICT FAIL, " +
+                "	[registered_date] DATETIME NOT NULL ON CONFLICT FAIL, " +
+                "	[mobile_no] VARCHAR(16) NOT NULL ON CONFLICT FAIL, " +
+                "	CONSTRAINT \"UNIQUE_MOBILE_NO\" UNIQUE ([mobile_no]) ON CONFLICT FAIL" +
+                ")";
         jdbcTemplate.update(sql);
     }
 
     public void createGroupTable() {
         System.out.println("Creating group table");
-        String sql = "CREATE TABLE IF NOT EXISTS [contact_group] (" + "	[group_id] INTEGER PRIMARY KEY ASC ON CONFLICT FAIL AUTOINCREMENT UNIQUE ON CONFLICT FAIL, " + "	[name] VARCHAR(128) NOT NULL ON CONFLICT FAIL, " + "	CONSTRAINT \"UNIQUE_NAME\" UNIQUE ([name]) ON CONFLICT FAIL" + ")";
+        String sql = "CREATE TABLE IF NOT EXISTS [contact_group] (" +
+                "	[group_id] INTEGER PRIMARY KEY ASC ON CONFLICT FAIL AUTOINCREMENT UNIQUE ON CONFLICT FAIL, " +
+                "	[name] VARCHAR(128) NOT NULL ON CONFLICT FAIL, " +
+                "	CONSTRAINT \"UNIQUE_NAME\" UNIQUE ([name]) ON CONFLICT FAIL" +
+                ")";
         jdbcTemplate.update(sql);
     }
 
     public void createContactGroupJoinTable() {
         System.out.println("Creating contact group join table");
-        String sql = "CREATE TABLE IF NOT EXISTS [contact_group_join] (" + "	[contact_id] INTEGER REFERENCES [contact](contact_id), " + "	[group_id] INTEGER REFERENCES [group](group_id), " + "	CONSTRAINT \"UNIQUE_CONTACT_GROUP\" UNIQUE ([contact_id], [group_id]) ON CONFLICT FAIL" + ")";
+        String sql = "CREATE TABLE IF NOT EXISTS [contact_group_join] (" +
+                "	[contact_id] INTEGER REFERENCES [contact](contact_id), " +
+                "	[group_id] INTEGER REFERENCES [group](group_id), " +
+                "	CONSTRAINT \"UNIQUE_CONTACT_GROUP\" UNIQUE ([contact_id], [group_id]) ON CONFLICT FAIL" +
+                ")";
         jdbcTemplate.update(sql);
     }
 }
