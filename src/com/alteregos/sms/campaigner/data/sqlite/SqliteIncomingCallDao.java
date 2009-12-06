70694df61ec8c534475a6988941d24327e9fe33b
johnemmanuel <johnemmanuel@localhost>
2009-12-06 22:37:13 +0530

    MODIFIED:Method signatures

--- src/com/alteregos/sms/campaigner/data/sqlite/SqliteIncomingCallDao.java
+++ src/com/alteregos/sms/campaigner/data/sqlite/SqliteIncomingCallDao.java
@@ -2,7 +2,6 @@ package com.alteregos.sms.campaigner.data.sqlite;
 
 import com.alteregos.sms.campaigner.data.dao.IncomingCallDao;
 import com.alteregos.sms.campaigner.data.dto.IncomingCall;
-import com.alteregos.sms.campaigner.data.dto.IncomingCallPk;
 import com.alteregos.sms.campaigner.data.exceptions.DaoException;
 import com.alteregos.sms.campaigner.data.mappers.IncomingCallRowMapper;
 import org.springframework.dao.DataAccessException;
@@ -21,13 +20,12 @@ public class SqliteIncomingCallDao extends BaseSqliteDao implements IncomingCall
 
     private final String TABLE_NAME = "incoming_call";
     private final String DEFAULT_SELECTORS = " call_id, receipt_date, gateway_id, caller_no, process ";
-
     private String findByIdQuery;
     private String findAllQuery;
     private String insertStmt;
     private String updateStmt;
 
-    public SqliteIncomingCallDao () {
+    public SqliteIncomingCallDao() {
         findByIdQuery = "SELECT" + DEFAULT_SELECTORS + "FROM " + TABLE_NAME + " WHERE call_id = ?";
         findAllQuery = "SELECT" + DEFAULT_SELECTORS + "FROM " + TABLE_NAME + " ORDER BY call_id";
         insertStmt = "INSERT INTO " + TABLE_NAME + "(" + DEFAULT_SELECTORS + ") VALUES(?,?,?,?,?)";
@@ -36,47 +34,47 @@ public class SqliteIncomingCallDao extends BaseSqliteDao implements IncomingCall
     }
 
     @Override
-    public IncomingCall findById (int incomingCallId) {
+    public IncomingCall findById(int incomingCallId) {
         IncomingCall call;
         try {
-            call = jdbcTemplate.queryForObject (findByIdQuery, new IncomingCallRowMapper (), incomingCallId);
+            call = jdbcTemplate.queryForObject(findByIdQuery, new IncomingCallRowMapper(), incomingCallId);
         } catch (DataAccessException e) {
-            throw new DaoException (e);
+            throw new DaoException(e);
         }
         return call;
     }
 
     @Override
-    public List<IncomingCall> findAll () {
+    public List<IncomingCall> findAll() {
         List<IncomingCall> calls;
         try {
-            calls = jdbcTemplate.query (findAllQuery, new IncomingCallRowMapper ());            
+            calls = jdbcTemplate.query(findAllQuery, new IncomingCallRowMapper());
         } catch (DataAccessException e) {
-            throw new DaoException (e);
+            throw new DaoException(e);
         }
         return calls;
     }
 
     @Override
-    public IncomingCallPk insert (IncomingCall call) {
+    public synchronized int insert(IncomingCall call) {
         int lastId;
         try {
-            jdbcTemplate.update (insertStmt, null, call.getReceiptDate (), call.getGatewayId (), call.getCallerNo (),
-                    call.isProcess ());
-            lastId = getLastInsertedId ();
+            jdbcTemplate.update(insertStmt, null, call.getReceiptDate(), call.getGatewayId(), call.getCallerNo(),
+                    call.isProcess());
+            lastId = getLastInsertedId();
         } catch (DataAccessException e) {
-            throw new DaoException (e);
+            throw new DaoException(e);
         }
-        return new IncomingCallPk (lastId);
+        return lastId;
     }
 
     @Override
-    public void update (IncomingCallPk pk, IncomingCall call) {
+    public synchronized void update(IncomingCall call) {
         try {
-            jdbcTemplate.update (updateStmt, call.getReceiptDate (), call.getGatewayId (), call.getCallerNo (),
-                    call.isProcess (), call.getIncomingCallId ());
+            jdbcTemplate.update(updateStmt, call.getReceiptDate(), call.getGatewayId(), call.getCallerNo(),
+                    call.isProcess(), call.getIncomingCallId());
         } catch (DataAccessException e) {
-            throw new DaoException (e);
+            throw new DaoException(e);
         }
     }
 }
