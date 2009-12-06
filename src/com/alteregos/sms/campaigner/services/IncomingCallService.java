a7df157872b68f746f3b396785a105d29cdeccd6
johnemmanuel <johnemmanuel@localhost>
2009-12-06 22:32:16 +0530

    MODIFIED: Insert & Update method signatures

--- src/com/alteregos/sms/campaigner/services/IncomingCallService.java
+++ src/com/alteregos/sms/campaigner/services/IncomingCallService.java
@@ -2,7 +2,6 @@ package com.alteregos.sms.campaigner.services;
 
 import com.alteregos.sms.campaigner.data.dao.IncomingCallDao;
 import com.alteregos.sms.campaigner.data.dto.IncomingCall;
-import com.alteregos.sms.campaigner.data.dto.IncomingCallPk;
 import java.util.List;
 
 /**
@@ -22,7 +21,6 @@ public class IncomingCallService {
     }
 
     public int newIncomingCall(IncomingCall call) {
-        IncomingCallPk pk = incomingCallDao.insert(call);
-        return pk != null ? pk.getIncomingCallId() : 0;
+        return incomingCallDao.insert(call);
     }
 }
