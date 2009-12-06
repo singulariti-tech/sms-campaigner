e72e55c587b8da0dba799403b70e381f8b4b75db
johnemmanuel <johnemmanuel@localhost>
2009-12-06 22:38:18 +0530

    MODIFIED:Method signatures

--- src/com/alteregos/sms/campaigner/data/sqlite/SqliteOutgoingMessageDao.java
+++ src/com/alteregos/sms/campaigner/data/sqlite/SqliteOutgoingMessageDao.java
@@ -3,9 +3,9 @@ package com.alteregos.sms.campaigner.data.sqlite;
 import com.alteregos.sms.campaigner.business.MessageStatus;
 import com.alteregos.sms.campaigner.data.dao.OutgoingMessageDao;
 import com.alteregos.sms.campaigner.data.dto.OutgoingMessage;
-import com.alteregos.sms.campaigner.data.dto.OutgoingMessagePk;
 import com.alteregos.sms.campaigner.data.exceptions.DaoException;
 import com.alteregos.sms.campaigner.data.mappers.OutgoingMessageRowMapper;
+import java.util.ArrayList;
 import org.springframework.dao.DataAccessException;
 
 import java.util.List;
@@ -21,9 +21,7 @@ import java.util.List;
 public class SqliteOutgoingMessageDao extends BaseSqliteDao implements OutgoingMessageDao {
 
     private final String TABLE_NAME = "outgoing_message";
-    private final String DEFAULT_SELECTORS = " outgoing_message_id, content, created_date, sent_date, errors, "
-            + "gateway_id, dst_port, src_port, recepient_no, sender_no, status_report, status, encoding, "
-            + "flash_message, priority, ref_no, type ";
+    private final String DEFAULT_SELECTORS = " outgoing_message_id, content, created_date, sent_date, errors, " + "gateway_id, dst_port, src_port, recepient_no, sender_no, status_report, status, encoding, " + "flash_message, priority, ref_no, message_type ";
     private String findByIdQuery;
     private String findAllQuery;
     private String findByStatus;
@@ -31,17 +29,11 @@ public class SqliteOutgoingMessageDao extends BaseSqliteDao implements OutgoingM
     private String updateStmt;
 
     public SqliteOutgoingMessageDao() {
-        findByIdQuery = "SELECT" + DEFAULT_SELECTORS + "FROM " + TABLE_NAME + " WHERE outgoing_message_id = ? "
-                + "ORDER BY outgoing_message_id ASC";
+        findByIdQuery = "SELECT" + DEFAULT_SELECTORS + "FROM " + TABLE_NAME + " WHERE outgoing_message_id = ? " + "ORDER BY outgoing_message_id ASC";
         findAllQuery = "SELECT" + DEFAULT_SELECTORS + "FROM " + TABLE_NAME + " ORDER BY outgoing_message_id ASC";
-        findByStatus = "SELECT" + DEFAULT_SELECTORS + "FROM " + TABLE_NAME + " WHERE status = ? "
-                + "ORDER BY outgoing_message_id ASC";
-        insertStmt = "INSERT INTO " + TABLE_NAME + " (" + DEFAULT_SELECTORS + ") "
-                + "VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
-        updateStmt = "UPDATE " + TABLE_NAME + " SET content = ?, created_date = ?, sent_date = ?, errors = ?, "
-                + "gateway_id = ?, dst_port = ?, src_port = ?, recepient_no = ?, sender_no = ?, status_report = ?, "
-                + "status = ?, encoding = ?, flash_message = ?, priority = ?, ref_no = ?, type = ? "
-                + "WHERE outgoing_message_id = ?";
+        findByStatus = "SELECT" + DEFAULT_SELECTORS + "FROM " + TABLE_NAME + " WHERE status = ? " + "ORDER BY outgoing_message_id ASC";
+        insertStmt = "INSERT INTO " + TABLE_NAME + " (" + DEFAULT_SELECTORS + ") " + "VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+        updateStmt = "UPDATE " + TABLE_NAME + " SET content = ?, created_date = ?, sent_date = ?, errors = ?, " + "gateway_id = ?, dst_port = ?, src_port = ?, recepient_no = ?, sender_no = ?, status_report = ?, " + "status = ?, encoding = ?, flash_message = ?, priority = ?, ref_no = ?, message_type = ? " + "WHERE outgoing_message_id = ?";
     }
 
     @Override
@@ -78,27 +70,52 @@ public class SqliteOutgoingMessageDao extends BaseSqliteDao implements OutgoingM
     }
 
     @Override
-    public OutgoingMessagePk insert(OutgoingMessage m) {
+    public synchronized int insert(OutgoingMessage m) {
         int lastId;
         try {
             jdbcTemplate.update(insertStmt, null, m.getContent(), m.getCreatedDate(), m.getSentDate(), m.getErrors(),
                     m.getGatewayId(), m.getDstPort(), m.getSrcPort(), m.getRecepientNo(), m.getSenderNo(),
-                    m.getStatus(), m.getEncoding(), m.isFlashMessage(), m.getPriority(), m.getReferenceNo(),
-                    m.getType());
+                    m.isStatusReport(), m.getStatus().toString(), m.getEncoding().toString(), m.isFlashMessage(),
+                    m.getPriority().toString(), m.getReferenceNo(), m.getType().toString());
             lastId = getLastInsertedId();
         } catch (DataAccessException e) {
             throw new DaoException(e);
         }
-        return new OutgoingMessagePk(lastId);
+        return lastId;
     }
 
     @Override
-    public void update(OutgoingMessagePk pk, OutgoingMessage m) {
+    public synchronized int[] insert(List<OutgoingMessage> messages) {
+        int[] counts = null;
+        try {
+            List<Object[]> batch = new ArrayList<Object[]>();
+            for (OutgoingMessage m : messages) {
+                Object[] oa = new Object[]{
+                    null, m.getContent(), m.getCreatedDate(), m.getSentDate(), m.getErrors(),
+                    m.getGatewayId(), m.getDstPort(), m.getSrcPort(), m.getRecepientNo(), m.getSenderNo(),
+                    m.isStatusReport(), m.getStatus().toString(), m.getEncoding().toString(), m.isFlashMessage(),
+                    m.getPriority().toString(), m.getReferenceNo(), m.getType().toString()
+                };
+                batch.add(oa);
+            }
+            counts = jdbcTemplate.batchUpdate(insertStmt, batch);
+        } catch (DataAccessException e) {
+            e.printStackTrace();
+            throw new DaoException(e);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new DaoException(e);
+        }
+        return counts;
+    }
+
+    @Override
+    public synchronized void update(OutgoingMessage m) {
         try {
             jdbcTemplate.update(updateStmt, m.getContent(), m.getCreatedDate(), m.getSentDate(), m.getErrors(),
                     m.getGatewayId(), m.getDstPort(), m.getSrcPort(), m.getRecepientNo(), m.getSenderNo(),
-                    m.getStatus(), m.getEncoding(), m.isFlashMessage(), m.getPriority(), m.getReferenceNo(),
-                    m.getType(), m.getOutgoingMessageId());
+                    m.isStatusReport(), m.getStatus().toString(), m.getEncoding().toString(), m.isFlashMessage(),
+                    m.getPriority().toString(), m.getReferenceNo(), m.getType().toString(), m.getOutgoingMessageId());
         } catch (DataAccessException e) {
             throw new DaoException(e);
         }
