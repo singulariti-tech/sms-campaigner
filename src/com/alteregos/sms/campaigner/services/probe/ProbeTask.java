3073cebc98a98374b59ed1815ec4700f9ccdeb64
johnemmanuel <johnemmanuel@localhost>
2010-01-14 16:54:24 +0530

    MODIFIED: Removed file comment.

--- src/com/alteregos/sms/campaigner/services/probe/ProbeTask.java
+++ src/com/alteregos/sms/campaigner/services/probe/ProbeTask.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.alteregos.sms.campaigner.services.probe;
 
 import com.alteregos.sms.campaigner.Main;
