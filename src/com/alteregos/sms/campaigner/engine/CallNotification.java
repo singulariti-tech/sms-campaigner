c1b5c0655f53434fa930ccc222834724c6717d61
johnemmanuel <johnemmanuel@localhost>
2010-01-14 15:16:06 +0530

    MODIFIED: Removed file comment.

--- src/com/alteregos/sms/campaigner/engine/CallNotification.java
+++ src/com/alteregos/sms/campaigner/engine/CallNotification.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.alteregos.sms.campaigner.engine;
 
 import com.alteregos.sms.campaigner.engine.receivers.CallReceiver;
