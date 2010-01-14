64c7327aebea591f8b831f830c518ba46e8dbdc3
johnemmanuel <johnemmanuel@localhost>
2010-01-14 20:00:54 +0530

    ADDED: Basic support for sqlite duplicate entry errors

--- src/com/alteregos/sms/campaigner/exceptions/ExceptionParser.java
+++ src/com/alteregos/sms/campaigner/exceptions/ExceptionParser.java
@@ -7,11 +7,12 @@ package com.alteregos.sms.campaigner.exceptions;
 public class ExceptionParser {
 
     //TODO This is not valid for Sqlite
-    private static final String DUPLICATE_ENTRY_CONSTRAINT = "com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException: Duplicate entry";
+    private static final String MYSQL_DUPLICATE_ENTRY_CONSTRAINT = "com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException: Duplicate entry";
+    private static final String SQLITE_DUPLICATE_ENTRY_CONSTRAINT = "Transaction is already completed - do not call commit or rollback more than once per transaction";
 
     public static ITaskResult getError(Exception ex) {
         String message = ex.getMessage();
-        if (message.contains(DUPLICATE_ENTRY_CONSTRAINT)) {
+        if (message.contains(SQLITE_DUPLICATE_ENTRY_CONSTRAINT)) {
             return new UnsuccessfulTaskResult(ResultMessage.DUPLICATE_ENTRY);
         }
         //TODO Log this so we can support it in future
