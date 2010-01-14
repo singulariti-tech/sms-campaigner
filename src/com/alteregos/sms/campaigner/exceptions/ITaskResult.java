f8ef9098b8a90b2803933339ad677ab9c0ae7010
johnemmanuel <johnemmanuel@localhost>
2010-01-14 16:49:19 +0530

    MODIFIED: Removed file comment.

--- src/com/alteregos/sms/campaigner/exceptions/ITaskResult.java
+++ src/com/alteregos/sms/campaigner/exceptions/ITaskResult.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.alteregos.sms.campaigner.exceptions;
 
 /**
@@ -9,8 +5,6 @@ package com.alteregos.sms.campaigner.exceptions;
  * @author John Emmanuel
  */
 public interface ITaskResult {
-
     ResultMessage getResultMessage();
-
     boolean isSuccessful();
 }
